f81c83186c638c312835f72cd767460afe0cd157
Leon Oostrum <l.oostrum@esciencecenter.nl>
2025-04-16 12:09:37 +0200

    Fix exe path

--- bin/run_remote.sh
+++ bin/run_remote.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/loostrum3/nsight
 
-#mkdir -p reports
-#nsys profile --trace cuda,openacc,mpi,osrt,ucx -f true -o reports/report6_mpi_remote_rank${SLURM_PROCID} /home/loostrum3/nsight/6_mpi
+mkdir -p reports
+nsys profile --trace cuda,openacc,mpi,osrt,ucx -f true -o reports/report6_mpi_remote_rank${SLURM_PROCID} $PWD/6_mpi
 
-export UCX_IB_GPU_DIRECT_RDMA=no
-nsys profile --trace cuda,openacc,mpi,osrt,ucx -f true -o reports/report6_mpi_remote_rank${SLURM_PROCID}_nordma /home/loostrum3/nsight/6_mpi
+#export UCX_IB_GPU_DIRECT_RDMA=no
+#nsys profile --trace cuda,openacc,mpi,osrt,ucx -f true -o reports/report6_mpi_remote_rank${SLURM_PROCID}_nordma $PWD/6_mpi
 
